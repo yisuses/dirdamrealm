2b6d5644bd76c59040298bcba08caf726bd4c52a
Saúl de León Guerrero <saul.deleon@bonhams.com>
2022-03-09 11:45:18 +0100

    feat: add theme font

--- packages/common/.storybook/preview.js
+++ packages/common/.storybook/preview.js
@@ -1,3 +1,6 @@
+import theme from '../src/theme/theme'
+import '@fontsource/roboto'
+
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
@@ -6,4 +9,21 @@ export const parameters = {
       date: /Date$/,
     },
   },
+  chakra: {
+    theme,
+  },
 }
+
+const StoryTemplate = ({ children, ...rest }) => (
+  <div style={{ margin: 10, marginTop: 40 }} {...rest}>
+    {children}
+  </div>
+)
+
+export const decorators = [
+  Story => (
+    <StoryTemplate>
+      <Story />
+    </StoryTemplate>
+  ),
+]
