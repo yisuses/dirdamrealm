0eb50e8d04b3c1a47166bb0017a24e0b980e5b72
Saúl de León Guerrero <sauldeleonguerrero@gmail.com>
2022-03-27 13:50:21 +0200

    feat: add properties to HeaderImage

--- packages/blog/src/components/HeaderImage/HeaderImage.tsx
+++ packages/blog/src/components/HeaderImage/HeaderImage.tsx
@@ -1,14 +1,61 @@
-import { Box } from '@chakra-ui/react'
+import { Box, Tag, TagLabel, Text, Flex, Divider, useBreakpointValue } from '@chakra-ui/react'
+import { format } from 'date-fns'
 import Image from 'next/image'
 
 interface HeaderImageProps {
-  src: string
+  imgSrc: string
+  title: string
+  subtitle: string
+  date: Date
+  categories: string[]
 }
 
-export function HeaderImage({ src }: HeaderImageProps) {
+export function HeaderImage({ imgSrc, categories, title, subtitle, date }: HeaderImageProps) {
+  const isMinWidthMd = useBreakpointValue({ md: true })
   return (
-    <Box height={{ base: 200, md: 400, lg: 600 }} position="relative">
-      <Image src={src} layout="fill" height={600} width="100%" objectFit="cover" />
+    <Box height={{ base: 250, md: 400, lg: 600 }} position="relative">
+      <Image src={imgSrc} layout="fill" height="600px" width="100%" objectFit="cover" />
+      <Box
+        position="absolute"
+        top={{ base: '70px', md: '50%' }}
+        ml={{ base: '15px', md: '70px' }}
+        mr={{ base: '15px', md: '70px' }}
+        maxWidth="530px"
+      >
+        <Flex gap="10px">
+          {categories.map((label, index) => {
+            return (
+              <Tag
+                key={`${label}-${index}`}
+                size={isMinWidthMd ? 'md' : 'sm'}
+                variant="outline"
+                boxShadow="none"
+                bgColor="blackAlpha.500"
+                mb={{ base: '8px', md: '10px', lg: '15px' }}
+              >
+                <TagLabel fontFamily="Roboto" textTransform="uppercase" color="white" fontSize={10} fontWeight={700}>
+                  {label}
+                </TagLabel>
+              </Tag>
+            )
+          })}
+        </Flex>
+        <Text
+          fontWeight={700}
+          fontSize={{ base: 'px', md: '26px', lg: '36px' }}
+          lineHeight={{ base: '26px', md: '36px', lg: '46px' }}
+          color="white"
+          textShadow={`0px 4px 3px rgb(0 0 0 / 40%), 0px 8px 13px rgb(0 0 0 / 10%), 0px 18px 23px rgb(0 0 0 / 10%);`}
+          mb={{ base: '8px', md: '10px', lg: '15px' }}
+        >
+          {title}
+        </Text>
+        <Flex alignItems="flex-start" color="white" fontSize="12px">
+          <Text>{format(date, 'dd.MM.yyyy')}</Text>
+          <Divider orientation="horizontal" w="45px" pt="9px" m="0 15px" opacity={1} />
+          <Text>{subtitle}</Text>
+        </Flex>
+      </Box>
     </Box>
   )
 }
--- packages/blog/src/pages/index.tsx
+++ packages/blog/src/pages/index.tsx
@@ -15,7 +15,13 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <MainLayout>
-        <HeaderImage src="https://images.unsplash.com/photo-1506477331477-33d5d8b3dc85?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=868&q=80" />
+        <HeaderImage
+          imgSrc="https://images.unsplash.com/photo-1506477331477-33d5d8b3dc85?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=868&q=80"
+          date={new Date()}
+          categories={['travel', 'lifestyle']}
+          title="Vacaciones en el Algarve. Una experiencia inolvidable."
+          subtitle="Un viaje por el Algarve y sus playas, descubriendo la paz del Atlántico. Sus gentes y su gastronomía de cerca."
+        />
       </MainLayout>
     </>
   )
