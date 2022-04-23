aa4866c40ced8b8bcc05e63a24874d137b812274
Saúl de León Guerrero <sauldeleonguerrero@gmail.com>
2022-04-23 13:05:13 +0200

    feat: improve querying

--- packages/blog/src/hooks/useGetCategories/useGetCategories.ts
+++ packages/blog/src/hooks/useGetCategories/useGetCategories.ts
@@ -1,15 +1,15 @@
 import { useTranslation } from 'next-i18next'
-import { useQuery } from 'react-query'
+import { useQueryClient } from 'react-query'
 
-import { getCategories } from 'api'
 import { seoName } from 'utils'
 
 export function useGetCategories(): { label: string; url: string }[] {
   const { t } = useTranslation('common')
-  const { data: categoriesData } = useQuery('categories', getCategories)
+  const queryClient = useQueryClient()
+  const categories = queryClient.getQueryData<CategoryResponse>('categories')
 
   return (
-    categoriesData?.data
+    categories?.data
       .filter(({ attributes }) => attributes.main)
       .map(({ attributes }) => ({
         label: t(`categories.${attributes.code}` as const),
