1ea558f59991ec99f72be06a7b0416d68ffeaf07
Saúl de León Guerrero <sauldeleonguerrero@gmail.com>
2022-04-25 21:59:59 +0200

    fix: postMapper

--- packages/blog/src/api/mapper/postMapper.ts
+++ packages/blog/src/api/mapper/postMapper.ts
@@ -1,16 +1,9 @@
 import { categoryMapper } from './categoryMapper'
 
 export const postMapper = (postEntity: StrapiDataItem<PostResponseEntity>): Post => {
-  const postAttributes = postEntity.attributes
   return {
     id: postEntity.id,
-    title: postAttributes.title,
-    summary: postAttributes.summary,
-    imgUrl: postAttributes.imgUrl,
-    publishedAt: postAttributes.publishedAt,
-    categories: postAttributes.categories.data.map(category => categoryMapper(category)),
-    content: postAttributes.content,
-    createdAt: postAttributes.createdAt,
-    updatedAt: postAttributes.updatedAt,
+    ...postEntity.attributes,
+    categories: postEntity.attributes.categories.data.map(category => categoryMapper(category)),
   }
 }
