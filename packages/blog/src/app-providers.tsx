c758abe493bfa2e77e21a060f034b2d76957c669
Saúl de León Guerrero <sauldeleonguerrero@gmail.com>
2022-04-26 10:44:13 +0200

    chore: update dependencies

--- packages/blog/src/app-providers.tsx
+++ packages/blog/src/app-providers.tsx
@@ -1,19 +1,11 @@
 import { ChakraProvider } from '@chakra-ui/react'
-import type { EmotionCache } from '@emotion/react'
-import { CacheProvider } from '@emotion/react'
 import { emotionTheme } from '@whe/common'
 import { AppProps } from 'next/app'
 import { ReactNode, useState } from 'react'
 import { DefaultOptions, Hydrate, MutationCache, QueryCache, QueryClient, QueryClientProvider } from 'react-query'
 
-import { createEmotionCache } from '@/core/nextjs/create-emotion-cache'
-
-// Client-side cache, shared for the whole session of the user in the browser.
-const clientSideEmotionCache = createEmotionCache()
-
 type AppProvidersProps = {
   children: ReactNode
-  emotionCache?: EmotionCache
   globalProps?: GlobalProps
   pageProps?: AppProps['pageProps']
   queryClientConfig?: {
@@ -23,23 +15,15 @@ type AppProvidersProps = {
   }
 }
 
-export const AppProviders = ({
-  children,
-  emotionCache = clientSideEmotionCache,
-  globalProps,
-  pageProps,
-  queryClientConfig,
-}: AppProvidersProps) => {
+export const AppProviders = ({ children, globalProps, pageProps, queryClientConfig }: AppProvidersProps) => {
   const [queryClient] = useState(() => new QueryClient(queryClientConfig))
   return (
-    <CacheProvider value={emotionCache}>
-      <ChakraProvider theme={emotionTheme}>
-        <QueryClientProvider client={queryClient}>
-          <Hydrate state={globalProps?.dehydratedState}>
-            <Hydrate state={pageProps?.dehydratedState}>{children}</Hydrate>
-          </Hydrate>
-        </QueryClientProvider>
-      </ChakraProvider>
-    </CacheProvider>
+    <ChakraProvider theme={emotionTheme}>
+      <QueryClientProvider client={queryClient}>
+        <Hydrate state={globalProps?.dehydratedState}>
+          <Hydrate state={pageProps?.dehydratedState}>{children}</Hydrate>
+        </Hydrate>
+      </QueryClientProvider>
+    </ChakraProvider>
   )
 }
