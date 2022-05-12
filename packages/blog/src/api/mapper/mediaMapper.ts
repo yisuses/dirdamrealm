3704f195b3821540d927ceaaf0dde7b950cf7b8f
Saúl de León Guerrero <sauldeleonguerrero@gmail.com>
2022-05-12 11:37:57 +0200

    feat: improve mapper

--- packages/blog/src/api/mapper/mediaMapper.ts
+++ packages/blog/src/api/mapper/mediaMapper.ts
@@ -1,3 +1,10 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+/* eslint-disable @typescript-eslint/no-unused-vars */
 export const mediaMapper = (mediaEntity: StrapiDataItem<MediaResponseEntity>): Media => {
-  return { id: mediaEntity.id, ...mediaEntity.attributes }
+  const {
+    id,
+    attributes: { provider_metadata, ...mediaEntityAttributes },
+  } = mediaEntity
+
+  return { id, ...mediaEntityAttributes }
 }
---
+++ packages/blog/src/api/types/media.d.ts
@@ -0,0 +1,60 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+type MediaFormat = {
+  ext: string
+  url: string
+  hash: string
+  mime: string
+  name: string
+  path: string | null
+  size: number
+  width: number
+  height: number
+  provider_metadata: {
+    public_id: string
+    resource_type: string
+  }
+}
+type FormatType = 'small' | 'medium' | 'large' | 'thumbnail'
+
+type MediaResponseEntity = {
+  name: string
+  alternativeText: string
+  caption: string
+  width: number
+  height: number
+  formats: Record<FormatType, MediaFormat>
+  hash: string
+  ext: string
+  mime: string
+  size: number
+  url: string
+  previewUrl: string | null
+  provider: string
+  provider_metadata: {
+    public_id: string
+    resource_type: string
+  }
+  createdAt: string
+  updatedAt: string
+}
+
+type MediaResponse = StrapiData<MediaResponseEntity>
+
+type Media = {
+  id: number
+  name: string
+  alternativeText: string
+  caption: string
+  width: number
+  height: number
+  formats: Record<FormatType, MediaFormat>
+  hash: string
+  ext: string
+  mime: string
+  size: number
+  url: string
+  previewUrl: string | null
+  provider: string
+  createdAt: string
+  updatedAt: string
+}
