02130307abc44d9529615b93794364618868a3df
Saúl de León Guerrero <sauldeleonguerrero@gmail.com>
2022-05-15 01:01:44 +0200

    fix: dependencies

--- packages/api/config/middlewares.js
+++ packages/api/config/middlewares.js
@@ -14,8 +14,8 @@ module.exports = [
         useDefaults: true,
         directives: {
           'connect-src': ["'self'", 'https:'],
-          'img-src': ["'self'", 'data:', 'blob:', 'dl.airtable.com', 'res.cloudinary.com'],
-          'media-src': ["'self'", 'data:', 'blob:', 'dl.airtable.com', 'res.cloudinary.com'],
+          'img-src': ["'self'", 'data:', 'blob:', 'dl.airtable.com', 'res.cloudinary.com', '*'],
+          'media-src': ["'self'", 'data:', 'blob:', 'dl.airtable.com', 'res.cloudinary.com', '*'],
           upgradeInsecureRequests: null,
         },
       },
