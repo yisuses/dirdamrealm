fec87a11c13ec3b3693a8e462f5deb7fa8ef5149
Saúl de León Guerrero <sauldeleonguerrero@gmail.com>
2022-06-01 08:58:16 +0200

    fix: next available format for image (#65)

--- packages/blog/src/utils/image.ts
+++ packages/blog/src/utils/image.ts
@@ -16,5 +16,22 @@ export function getImageUrlFromMedia({
   format = 'large',
   fallback = '/images/WElogo.png',
 }: GetImageUrlFromMediaParams) {
-  return media ? media.formats[format].url : fallback || '/images/WElogo.png'
+  try {
+    if (!media) return fallback || '/images/WElogo.png'
+
+    const preferredFormat = media.formats[format]
+    if (preferredFormat) return preferredFormat.url
+
+    //search for the following format available
+    const nextAvailableFormatKeys = Object.keys(media.formats) as FormatType[]
+    const nextAvailableFormat = nextAvailableFormatKeys.find(formatKey => media.formats[formatKey])
+    if (!nextAvailableFormat) return media.url
+
+    const nextAvailableFormatData = media.formats[nextAvailableFormat]
+    if (!nextAvailableFormatData) return media.url
+    return nextAvailableFormatData.url
+  } catch {
+    console.error(`There was an error loading the image. ${media ? media.id : ''}, ${format}, ${fallback}`)
+    return '/images/WElogo.png'
+  }
 }
