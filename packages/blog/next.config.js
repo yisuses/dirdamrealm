29a1b8f8db2d8527f7e9b73fe19299504a0aa040
Saúl de León Guerrero <sauldeleonguerrero@gmail.com>
2022-12-19 14:06:45 +0100

    chore: remove unused property

--- packages/blog/next.config.js
+++ packages/blog/next.config.js
@@ -89,8 +89,6 @@ const nextConfig = {
   // Replace terser by swc
   swcMinify: true,
   experimental: {
-    serverComponents: false, // buggy with sentry
-
     // Prefer loading of ES Modules over CommonJS
     // @link {https://nextjs.org/blog/next-11-1#es-modules-support|Blog 11.1.0}
     // @link {https://github.com/vercel/next.js/discussions/27876|Discussion}
