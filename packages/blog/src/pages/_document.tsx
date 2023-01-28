22fc1b0a42e69968c462a8b043af9000b8fe9952
Saúl de León Guerrero <sauldeleonguerrero@gmail.com>
2023-01-28 08:56:58 +0100

    fix: crossOrigin property

--- packages/blog/src/pages/_document.tsx
+++ packages/blog/src/pages/_document.tsx
@@ -8,7 +8,7 @@ export default class Document extends NextDocument {
       <Html lang="es">
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
           <link
             href="https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400;0,600;0,700;1,400;1,600;1,700&family=Roboto:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&display=swap"
             rel="stylesheet"
