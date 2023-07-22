ff93bae80b3d272548affd33a7a91182f2884f4d
Saúl de León Guerrero <sauldeleonguerrero@gmail.com>
2023-07-22 14:43:12 +0200

    feat: update sitemaps

--- packages/blog/src/pages/sitemap.xml.ts
+++ packages/blog/src/pages/sitemap.xml.ts
@@ -4,7 +4,13 @@ import { publicUrl } from '@blog/utils/generateUrl/generateUrl'
 
 function generateSiteMap() {
   return `<?xml version="1.0" encoding="UTF-8"?>
-   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <urlset
+    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+    xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd
+    http://www.w3.org/1999/xhtml http://www.w3.org/2002/08/xhtml/xhtml1-strict.xsd"
+    xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+    xmlns:xhtml="http://www.w3.org/2002/08/xhtml/xhtml1-strict.xsd"   
+  >
      <url>
       <loc>${publicUrl('/sitemap/page.xml')}</loc>
      </url>
